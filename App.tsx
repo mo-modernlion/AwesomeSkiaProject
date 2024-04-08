e7320b5771149f9ad12bd9b7c2bd73f055aa6838
junmo yang <jmyang-lion@github.com>
2024-04-08 18:20:43 +0900

    inheritance 2

--- App.tsx
+++ App.tsx
@@ -1,4 +1,10 @@
-import {Canvas, Circle, Group} from '@shopify/react-native-skia';
+import {
+  Canvas,
+  Circle,
+  Group,
+  LinearGradient,
+  vec,
+} from '@shopify/react-native-skia';
 import React from 'react';
 import {View} from 'react-native';
 
@@ -10,11 +16,20 @@ function App(): JSX.Element {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Canvas style={{width, height}}>
-        <Group color="lightblue">
-          <Circle cx={r} cy={r} r={r} />
-          <Group style="stroke" strokeWidth={10}>
-            <Circle cx={3 * r} cy={3 * r} r={r} />
-          </Group>
+        <Circle cx={r} cy={r} r={r}>
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(2 * r, 2 * r)}
+            colors={['#00ff87', '#60efff']}
+          />
+        </Circle>
+        <Group>
+          <LinearGradient
+            start={vec(2 * r, 2 * r)}
+            end={vec(4 * r, 4 * r)}
+            colors={['#0061ff', '#60efff']}
+          />
+          <Circle cx={3 * r} cy={3 * r} r={r} />
         </Group>
       </Canvas>
     </View>
